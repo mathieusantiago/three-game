d8fa88f1fb0b8b78665a37d9702d2f2c0f874acc
mathieusantiago <mathieusantiago21@gmail.com>
2023-03-24 12:35:30 +0100

    add paved for market place

--- src/Game/PavedGroup/PavedGroup.jsx
+++ src/Game/PavedGroup/PavedGroup.jsx
@@ -63,6 +63,57 @@ const PavedGroup = () => {
       return [x, -0.09, -21.9];
     });
 
+    const DirtXPositions11 = Array.from({ length: 15 }, (_, index) => {
+      const x = -33 + index * 2.0;
+      return [5, -0.09, x];
+    });
+
+    const DirtYPositions12 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, -7.5];
+    });
+    const DirtYPositions13 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, -5.5];
+    });
+    const DirtYPositions14 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, -3.5];
+    });
+    const DirtYPositions15 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, -1.5];
+    });
+    const DirtYPositions16 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, 0.5];
+    });
+
+    const DirtYPositions17 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, 2.5];
+    });
+
+    const DirtYPositions18 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, 2.5];
+    });
+    const DirtYPositions19 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, 4.5];
+    });
+    const DirtYPositions20 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, 6.5];
+    });
+    const DirtYPositions21 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, 8.5];
+    });
+    const DirtYPositions22 = Array.from({ length: 15 }, (_, index) => {
+      const x = -8.5 + index * 2.0;
+      return [x, -0.09, 10.5];
+    });
     return (
         <>
             <Dirt positions={DirtXPositions1}/>
@@ -81,6 +132,21 @@ const PavedGroup = () => {
             <Dirt positions={DirtYPositions8}/>
             <Dirt positions={DirtYPositions9}/>
             <Dirt positions={DirtYPositions10}/>
+            {/* marquet place */}
+            <Dirt positions={DirtXPositions11}/>
+            <Dirt positions={DirtYPositions12}/>
+            <Dirt positions={DirtYPositions13}/>
+            <Dirt positions={DirtYPositions14}/>
+            <Dirt positions={DirtYPositions15}/>
+            <Dirt positions={DirtYPositions16}/>
+            <Dirt positions={DirtYPositions17}/>
+            <Dirt positions={DirtYPositions18}/>
+            <Dirt positions={DirtYPositions19}/>
+            <Dirt positions={DirtYPositions20}/>
+            <Dirt positions={DirtYPositions21}/>
+            <Dirt positions={DirtYPositions22}/>
+
+
         </>
     );
 };
